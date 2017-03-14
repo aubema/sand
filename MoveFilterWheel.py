aebd1e0a3d688d1ab1f2771e8d8300c67c3b70ca
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2017-03-14 14:21:27 -0400

    filter movement

--- MoveFilterWheel.py
+++ MoveFilterWheel.py
@@ -15,7 +15,7 @@ servo = maestro.Controller()
 servo.setAccel(chan,4) # set acceleration
 servo.setSpeed(chan,30) # set speed
 servo.setTarget(chan,4000) # go to 4000
- time.sleep(3)
+time.sleep(3)
 servo.setTarget(chan,pos) # move to filter pos
 servo.close
 
