1a028b78337f2d37127811fef46c2956c939d7ed
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2017-03-14 14:07:06 -0400

    filter movement

--- MoveFilterWheel.py
+++ MoveFilterWheel.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 # usage MoveFilterWheel.py position channel
 import maestro
-pos = input()
-chan = input()
+import sys
+import argparse
+import time
+# parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('p', help='enter position', type=int)
+parser.add_argument('c', help='enter channel', type=int)
+args = parser.parse_args()
+pos = args.p
+chan = args.c
 servo = maestro.Controller()
 servo.setAccel(chan,4) # set acceleration
 servo.setSpeed(chan,4) # set speed
 servo.setTarget(chan,4000) # go to 4000
+time.sleep(10)
 servo.setTarget(chan,pos) # move to filter pos
 servo.close
 
