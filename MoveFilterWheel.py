3068a85fff811fd1e5f9ff36352325a44019fde9
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2017-03-16 13:40:15 -0400

    set park position

--- MoveFilterWheel.py
+++ MoveFilterWheel.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# usage MoveFilterWheel.py position channel
+# usage MoveFilterWheel.py position channel offset
 import maestro
 import sys
 import argparse
@@ -8,13 +8,15 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('p', help='enter position', type=int)
 parser.add_argument('c', help='enter channel', type=int)
+parser.add_argument('o', help='enter park position', type=int)
 args = parser.parse_args()
 pos = args.p
 chan = args.c
+park = args.o
 servo = maestro.Controller()
 servo.setAccel(chan,4) # set acceleration
 servo.setSpeed(chan,50) # set speed
-servo.setTarget(chan,4000) # go to 4000
+servo.setTarget(chan,park) # go to park position
 time.sleep(3)
 servo.setTarget(chan,pos) # move to filter pos
 servo.close
