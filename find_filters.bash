47c853570ceafdc5b672536d8f5ee1ccd9f198f4
graphycs1 <graphycs1@localhost>
2017-04-28 10:48:50 -0400

    find_filters

--- find_filters.bash
+++ find_filters.bash
@@ -63,9 +63,10 @@ while [ $scanpoint -le $maxpoint ]
 # %> b=${a//0/}  
 read toto sb toto toto toto toto < toto.tmp
 
-  echo $sb | sed 's/\./ /g' | sed "s/^0*\([1-9]\)/\1/;s/^0*$/0/" > toto.tmp
+  echo $sb | sed 's/\./ /g' | let $sb=${sb//0/} > toto.tmp
   read sbe sbd toto < toto.tmp
-#let $sb=${sb//0/}
+#
+#sed "s/^0*\([1-9]\)/\1/;s/^0*$/0/"
   let sb=(sbe*100+sbd)/1
 
   echo "finding filtre #: " $n       "pos: "$scanpoint       "magnitude: "$sb"m"
