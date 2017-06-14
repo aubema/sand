192164c21edb6db7c67ecb0265962a6c2f949620
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2017-06-14 11:56:53 -0400

    not tested version

--- photom2spectrum.bash
+++ photom2spectrum.bash
@@ -27,12 +27,13 @@ fi
 # number of points to fit
 npts=4
 #
-outname=
+outname=$1"analysed"
 gplotfile=
 # determine the number of lines of the photometer file
 nlines=`grep -c "" $1`
 npts=4
 nl=0
+echo "Date Time Rad_Hg_405 Rad_Hg_436 Rad_Na_498 Rad_Hg_546 Rad_OI_557 Rad_Na_569 Rad_OI_630 Rad_error Stars_polynomial" > $outname
 while [ $nl -le $nlines ]
 do let nl=nl+1
    cat $1 | head -$nl | tail -1 > dataline.tmp
@@ -118,29 +119,14 @@ do let nl=nl+1
    line8=`/bin/echo "scale=25;"$b8"-"$contb8 | /usr/bin/bc`
    line9=`/bin/echo "scale=25;"$b9"-"$contb9 | /usr/bin/bc`
    line10=`/bin/echo "scale=25;"$b10"-"$contb10 | /usr/bin/bc`
-   line11=`/bin/echo "scale=25;"$b10"-"$contb10 | /usr/bin/bc`
-
-
-
-
-rm -f $outname
-
-
-
-   # soustraction du continu
-   while read line
-   do echo $line | sed -e 's/E+/\*10\^/g' | sed -e 's/E-/\*10\^-/g' > ligne.tmp
-      read lambda intens < ligne.tmp
-      formule=`echo $formule | sed -e 's/x/'$lambda'/g'`
-      bg=`/bin/echo "scale=25;"$formule | /usr/bin/bc`
-      newint=`/bin/echo "scale=25;"$intens"-1.0*"$bg | /usr/bin/bc`
-      echo $lambda $newint  >> $outname
-   done < $1
-
+   line11=`/bin/echo "scale=25;"$b11"-"$contb11 | /usr/bin/bc`
+# estimating error with line1 line3 line5 line10
+   error=`/bin/echo "scale=25;sqrt(("$line1"^2.0+"$line3"^2.0+"$line5"^2.0+"$line10"^2.0)/4.0)" | /usr/bin/bc`
+# writing output
+   echo $dat $tim $line11 $line2 $line4 $line6 $line7 $line8 $line9 $error $a "+ " $b " xlambda + " $c " xlambda^2" >> $outname
 # fin du while
 done
 
 # cleaning
-rm -f var.txt
-rm -f fit.txt
-rm -f fit.log
+rm -f dataline.tmp
+rm -f continium.tmp
