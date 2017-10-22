4c84a1ac0cc8a8495561f4335792f227ff4c37f4
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2017-10-21 22:01:41 -0400

    code for reading sqm

--- read_archive_sqm.bash
+++ read_archive_sqm.bash
@@ -4,7 +4,7 @@ sqmip="192.168.0.10"
 read sqm < sqmdata.tmp
 /bin/echo $sqm | sed 's/,/ /g' | sed 's/m//g' | sed 's/\ 00000/\ /g' | sed 's/\ 0000/\ /g'| sed 's/\ 000/\ /g'| sed 's/\ 00/\ /g'| sed 's/\ 0/\ /g'> toto.tmp
 read toto sb toto toto intime temp toto  < toto.tmp
-corr_sqm=`/bin/echo "scale=5;"$sb"-"0.11" |/usr/bin/bc -l`
+corr_sqm=`/bin/echo "scale=5;"$sb"-0.11" |/usr/bin/bc -l`
 y=`/bin/date -u +%Y`
 mo=`/bin/date -u +%m`
 j=`/bin/date -u +%d`
@@ -13,5 +13,4 @@ h=`/bin/date -u +%H`
 s=`/bin/date -u +%S`
 /bin/mkdir /var/www/html/DATA-SAND/SQM-Saint-Camille/$y
 /bin/mkdir /var/www/html/DATA-SAND/SQM-Saint-Camille/$y/$mo
-/bin/mkdir /var/www/html/DATA-SAND/SQM-Saint-Camille/$y/$mo/$j
-/bin/echo $y $mo $j $h $m $s $corr_sqm $intime $temp >> /var/www/html/DATA-SAND/SQM-Saint-Camille/$y/$mo/$j/data-$y-$mo-$j.txt
+/bin/echo $y $mo $j $h $m $s $corr_sqm $intime $temp >> /var/www/html/DATA-SAND/SQM-Saint-Camille/$y/$mo/data-$y-$mo-$j.txt
